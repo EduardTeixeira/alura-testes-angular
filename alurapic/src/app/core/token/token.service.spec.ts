88b6839d959a4ac3d400ccf6ad067e1f946ccfe1
EduardTeixeira <eduard.teixeira@adsrock.com.br>
2020-11-04 13:25:09 -0300

    testes de serviços, token

--- alurapic/src/app/core/token/token.service.spec.ts
+++ alurapic/src/app/core/token/token.service.spec.ts
@@ -1,18 +1,35 @@
 import { TokenService } from './token.service';
 
-describe('O serviço TokenService', ()=>{
+describe('O serviço TokenService', () => {
 
-    it('deve ser instanciado', ()=>{
+    let token;
+    let service;
+
+    it('deve ser instanciado', () => {
         const service = new TokenService();
         expect(service).toBeTruthy();
     })
 
-    it('deve guardar um token', ()=>{
-        const token = 'testToken';
-        const service = new TokenService();
+    it('deve guardar um token', () => {
         service.setToken(token);
         expect(service.hasToken()).toBeTruthy();
         expect(service.getToken()).toBe('testToken');
+    })
+
+    it('Deve remover um token', () => {
+        service.setToken(token);
+        service.removeToken();
+        expect(service.hasToken()).toBeFalsy();
+        expect(service.getToken()).toBeFalsy();
+    })
+
+    afterEach(() => {
+        localStorage.clear();
+    })
+
+    beforeEach(() => {
+        token = 'testToken';
+        service = new TokenService();
     })
 
 })
